4a0bf2f1c4bdaa9cfb3ce9cb8d22cbb3fe165a8d
Nguyen Phuc Thien An <npta97@gmail.com>
2019-04-25 10:49:29 +0700

    Add Alert service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,11 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { ProgressAnimationType, ToastrModule } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 
 import { SharedModule } from '../shared/shared.module';
+import { AlertService } from './services/alert.service';
 import { AuthService } from './services/auth.service';
 
 export function tokenGetter() {
@@ -20,16 +22,25 @@ const jwtOptions = {
   }
 };
 
+const toastrOptions = {
+  timeOut: 5000,
+  positionClass: 'toast-bottom-right',
+  progressBar: true,
+  progressAnimation: 'increasing' as ProgressAnimationType
+};
+
 @NgModule({
   imports: [
     HttpClientModule,
     RouterModule.forChild([]),
     BrowserAnimationsModule,
     JwtModule.forRoot(jwtOptions),
+    ToastrModule.forRoot(toastrOptions),
     SharedModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    AlertService
   ]
 })
 export class CoreModule {
--- src/app/public/components/header/header.component.ts
+++ src/app/public/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/core/services/alert.service';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -11,7 +12,8 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private alertService: AlertService) { }
 
   ngOnInit() {
   }
@@ -22,6 +24,7 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+    this.alertService.info('Logout successfully');
     this.router.navigate(['/']);
   }
 
